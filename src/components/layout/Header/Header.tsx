745c4fd8b43f1aab332822d795d07076433071b1
gubmax <gubyshev@gmail.com>
2020-04-01 11:20:59 +0300

    Add sync btn to Header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -2,15 +2,21 @@ import React, { FC, useMemo, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 
 import { setBooleanItemToLocalStorage } from 'src/helpers'
+import { useRequest } from 'src/hooks'
 import { useStore } from 'src/store'
-import { SearchField } from 'src/components/elements'
+import { SearchField, Loader } from 'src/components/elements'
 import { ReactComponent as MenuIcon } from 'src/static/images/icons/menu-24px.svg'
+import { ReactComponent as RefreshIcon } from 'src/static/images/icons/refresh-24px.svg'
 import { ReactComponent as ProfileIcon } from 'src/static/images/icons/profile-24px.svg'
 import { HeaderProps } from './Header.interface'
 import s from './Header.module.scss'
 
 const Header: FC<HeaderProps> = ({ iconWithLink }) => {
   const [{ showSidebar }, { toggleSidebar }] = useStore()
+  const [{ isLoading }, fetchSync] = useRequest<true>({
+    url: '/sync',
+    preload: true,
+  })
 
   const toggleSidebarMode = useCallback(() => {
     setBooleanItemToLocalStorage('showSidebar', !showSidebar)
@@ -42,9 +48,19 @@ const Header: FC<HeaderProps> = ({ iconWithLink }) => {
       : menuIcon
   ), [iconWithLink, menuIcon])
 
+  const syncTemplate = useMemo(() => (
+    isLoading
+      ? (
+        <span className={s.syncLoader}>
+          <Loader />
+        </span>
+      )
+      : <RefreshIcon className={s.icon} onClick={fetchSync} />
+  ), [fetchSync, isLoading])
+
   return useMemo(() => (
     <header className={s.header}>
-      { menuIconTemplate }
+      {menuIconTemplate}
       <span className={s.title}>
         <span>Task</span>
         <span className={s.titleHighlight}>H</span>
@@ -52,10 +68,11 @@ const Header: FC<HeaderProps> = ({ iconWithLink }) => {
       </span>
       <SearchField className={s.search} />
       <div className={s.iconsBox}>
+        {syncTemplate}
         <ProfileIcon className={s.icon} />
       </div>
     </header>
-  ), [menuIconTemplate])
+  ), [menuIconTemplate, syncTemplate])
 }
 
 export { Header }

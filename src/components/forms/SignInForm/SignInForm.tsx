b19181bde42186547f132182f3404e7c30a913f3
gubmax <gubyshev@gmail.com>
2020-04-06 14:05:25 +0300

    Add loader for submit btn in sign-in form

--- src/components/forms/SignInForm/SignInForm.tsx
+++ src/components/forms/SignInForm/SignInForm.tsx
@@ -5,12 +5,19 @@ import { Block, Button } from 'src/components/elements'
 import s from './SignInForm.module.scss'
 
 const SignInForm: FC = () => {
-  const [, { signIn }] = useAuth()
+  const [{ isLoading }, { signIn }] = useAuth()
 
   return (
     <Block className={s.wrapper}>
       <span className={s.logo}>H</span>
-      <Button primary large onClick={signIn}>Войти</Button>
+      <Button
+        primary
+        large
+        loading={isLoading}
+        onClick={signIn}
+      >
+        Войти
+      </Button>
     </Block>
   )
 }

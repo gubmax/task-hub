a5041dc2ccc6921a419855a34d9e5f03198d5cdf
gubmax <gubyshev@gmail.com>
2020-05-13 21:56:55 +0300

    Set className to wrapper in Block

--- src/components/elements/Block/Block.tsx
+++ src/components/elements/Block/Block.tsx
@@ -1,13 +1,14 @@
 import React, { FC, memo } from 'react'
 
+import { cn } from 'src/helpers'
 import { BlockProps } from './Block.types'
 import s from './Block.module.scss'
 
 const Block: FC<BlockProps> = memo(({ className, children, title, text }) => (
-  <div className={s.wrapper}>
+  <div className={cn(s.wrapper, className)}>
     { title && <span className={s.title}>{title}</span> }
     { text && <span className={s.text}>{text}</span> }
-    <div className={`${s.block} ${className || ''}`}>{children}</div>
+    <div className={s.block}>{children}</div>
   </div>
 ))
 

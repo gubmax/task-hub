12273e9766b96993aa5c4339337e09745dc458e9
gubmax <gubyshev@gmail.com>
2020-05-14 00:12:28 +0300

    Refactor. Update List layout

--- src/components/elements/List/List.tsx
+++ src/components/elements/List/List.tsx
@@ -1,39 +1,36 @@
 import React, {
-  FC, memo, useCallback, cloneElement,
-  Children, ReactElement,
+  FC, memo, useMemo, useCallback,
+  cloneElement, Children, ReactElement,
 } from 'react'
 
+import { cn } from 'src/helpers'
 import { ListProps } from './List.types'
 import s from './List.module.scss'
 
 const List: FC<ListProps> = memo(({ className, children }) => {
-  const addClassToChild = useCallback(
-    (child: ReactElement) => {
-      const { className: childClassName } = child.props || {}
-
-      const props = {
-        className: childClassName !== undefined
-          ? `${s.listItem} ${childClassName}`
-          : s.listItem,
-      }
-
-      return (
-        <div className={s.listItemWrapper}>
-          {cloneElement(child, props)}
-        </div>
-      )
-    },
+  const addClassName = useCallback(
+    (child: ReactElement) => cloneElement(child, {
+      className: cn(s.listItem, child.props.className)
+    }),
     [],
   )
 
+  const itemsTemplate = useMemo(
+    () => (
+      Children.map(children, (child: ReactElement, index) => (
+        <>
+          {addClassName(child)}
+          { index !== children.length - 1 && <hr className={s.separator} />}
+        </>
+      ))
+    ),
+    [addClassName, children]
+  )
+
   return (
-    <>
-      <div className={className || ''}>
-        {
-          Children.map(children, (child: ReactElement) => addClassToChild(child))
-        }
-      </div>
-    </>
+    <div className={className || ''}>
+      {itemsTemplate}
+    </div>
   )
 })
 
